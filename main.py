914bfd7e5630f9a928ba54f0de5dc370d7078c65
Sayyodbek <sarvinoztogayeva603@gmail.com>
2025-09-28 13:21:53 +0500

    my first db

---
+++ main.py
@@ -0,0 +1,29 @@
+# --1. vazifa
+# SELECT *
+# FROM products
+#
+#
+# --2. vazifa
+# SELECT contact_name, city
+# FROM customers
+#
+#
+# --3. vazifa
+# SELECT order_id, order_date, shipped_date, freight
+# FROM orders
+#
+# --4. vaifa
+# SELECT city
+# FROM customers
+#
+# --5. vazifa
+# SELECT city, country
+# FROM customers
+#
+# --6. vazifa
+# SELECT COUNT(*)
+# FROM customers;
+#
+# --7. vazfa
+# SELECT COUNT(country)
+# FROM customers
